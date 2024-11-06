652cb509cbd9516f4018429761fca997c6abde62
xmlim <122072026+xmlim@users.noreply.github.com>
2024-11-06 20:56:13 +0800

    更新 config.py

--- config.py
+++ config.py
@@ -43,18 +43,6 @@ url_blacklist = [
     "iptv.luas.edu.cn"
 ]
 
-announcements = [
-    {
-        "channel": "公告",
-        "entries": [
-            {"name": "请阅读", "url": "https://liuliuliu.tv/api/channels/1997/stream", "logo": "http://175.178.251.183:6689/LR.jpg"},
-            {"name": "yuanzl77.github.io", "url": "https://liuliuliu.tv/api/channels/233/stream", "logo": "http://175.178.251.183:6689/LR.jpg"},
-            {"name": "更新日期", "url": "https://gitlab.com/lr77/IPTV/-/raw/main/%E4%B8%BB%E8%A7%92.mp4", "logo": "http://175.178.251.183:6689/LR.jpg"},
-            {"name": None, "url": "https://gitlab.com/lr77/IPTV/-/raw/main/%E8%B5%B7%E9%A3%8E%E4%BA%86.mp4", "logo": "http://175.178.251.183:6689/LR.jpg"}
-        ]
-    }
-]
-
 epg_urls = [
     "https://live.fanmingming.com/e.xml",
     "http://epg.51zmt.top:8000/e.xml",
